528625764a83e79d037d93c15836d66269cb6dda
Thomas <thomas.franken.nl@gmail.com>
2023-12-03 23:05:00 +0100

    alles herschreven

--- Reclame.py
+++ Reclame.py
@@ -1,8 +1,35 @@
+from algemene_functies import mijn_functie_2
+
 def aanbieding_1(smaak, prijs, korting):
     verkoop_prijs = prijs * (1-korting)
     print(f"emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} voor {verkoop_prijs} euro.")
+    return
 
-aanbieding_1("aardbei", 4, 0.1)  
+def inkomsten_totaal(inkomsten, BTW):
+    inkomsten = a + b + c + d + e + f + g
+    BTW_2 = inkomsten * BTW
+    print(f"het totaal van alle inkomsten van deze week is {inkomsten} euro, waarover {BTW_2} euro btw betaald dient te worden.")
+    return
+
+def laag_en_hoog(mijn_lijst):
+    fun_min = min(mijn_lijst)
+    fun_max = max(mijn_lijst)
+    return fun_min, fun_max
+
+def gemiddelde(mijn_lijst):
+    bedrag = ((sum(mijn_lijst))/len(mijn_lijst))
+    return bedrag
+       
+def meervoudig(invoer_lijst):
+    result = laag_en_hoog(invoer_lijst)
+    print(result)    
+    return result
+    
+def combinatie(invoerlijst_2):
+    korte_lijst = meervoudig(invoerlijst_2)
+    uitkomst = mijn_functie_2(korte_lijst)
+    return uitkomst
+    
 
 
 a = 220
@@ -13,11 +40,18 @@ e = 205
 f = 90
 g = 345
 
-def inkomsten_totaal(inkomsten, BTW):
-    inkomsten = a + b + c + d + e + f + g
-    BTW_2 = inkomsten * BTW
-    print(f"het totaal van alle inkomsten van deze week is {inkomsten} euro, waarover {BTW_2} euro btw betaald dient te worden.")
-    
+fun_min, fun_max = laag_en_hoog([220, 430, 125, 160, 205, 90, 345])
+print([fun_min, fun_max])
+
+variabelenlijst = [10, 5, 3, 2, 1, 2, 9]
+meervoudig(variabelenlijst)
+
+bedrag = gemiddelde([220, 430, 125, 160, 205, 90, 345])
+print(f"De gemiddelde inkomsten van deze week zijn {bedrag} euro")
+
+aanbieding_1("aardbei", 4, 0.1)  
+
 inkomsten_totaal(0, 0.09)
 
-
+uitkomst = combinatie(variabelenlijst)
+print(uitkomst)
--- algemene_functies.py
+++ algemene_functies.py
@@ -1,52 +1,15 @@
-a = 2
-b = 4
-c = 8
-d = 12
+def mijn_functie_1(lange_lijst):
+    a, b, c, d = lange_lijst
+    return a*a, b*b, c*c, d*d 
 
-def mijn_functie_1():
-    global a
-    x = a * a   
-    print(x)
-    
-    global b
-    y = b * b
-    print(y)
-    
-    global c
-    z = c * c
-    print(z)
-    
-    global d
-    q = d * d
-    print(q)
-    
-# mijn_functie_1()
+print(mijn_functie_1([2, 4, 8, 12]))
 
-e = 12
-f = 3
-g = 2
-h = 10
-i = 5
-j = 100
-k = 20
 
-def mijn_functie_2():
-    ResultA = e + f
-    ResultB = e - f
-    ResultC = e * f
-    ResultD = e / f
-    ResultE = e + g
-    ResultF = e - g
-    ResultG = e * g
-    ResultH = e / g
-    ResultI = h + i
-    ResultJ = h - i
-    ResultK = h * i
-    ResultL = h / i
-    ResultM = j + k
-    ResultN = j - k
-    ResultO = j * k
-    ResultP = j / k
-    print(ResultA, ResultB, ResultC, ResultD, ResultE, ResultF, ResultG, ResultH, ResultI, ResultJ, ResultK, ResultL, ResultM, ResultN, ResultO, ResultP)
-    
-mijn_functie_2()
+def mijn_functie_2(korte_lijst):
+    a, b = korte_lijst
+    return a+b, a-b, a*b, a/b
+
+print(mijn_functie_2([12,3]))
+print(mijn_functie_2([12,2]))
+print(mijn_functie_2([10,5]))
+print(mijn_functie_2([100,20]))
